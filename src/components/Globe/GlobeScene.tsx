1c53a9a20957ecd2682f20652917ffaf92b366f8
atek <53567837+DEV-industry@users.noreply.github.com>
2026-04-25 09:34:59 +0200

    feat: implement express proxy & globe UI

--- src/components/Globe/GlobeScene.tsx
+++ src/components/Globe/GlobeScene.tsx
@@ -11,8 +11,8 @@ import { useGeocode } from '../../hooks/useGeocode';
 import { getStakeTier, getTierColor, getTierRadius, getTierAltitude } from '../../utils/formatters';
 import type { Validator, ValidatorWithGeo } from '../../types';
 
-const GLOBE_IMAGE = 'https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg';
-const NIGHT_SKY = 'https://unpkg.com/three-globe/example/img/night-sky.png';
+const GLOBE_IMAGE = 'https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/assets/earth-dark.jpg';
+const NIGHT_SKY = 'https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/assets/night-sky.png';
 
 interface GlobeSceneProps {
   validators: Validator[];
